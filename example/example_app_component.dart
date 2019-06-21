3fe44246c3c377ec5625016bc473cf5fd0a84a7c
kuan <yuankuan@gmail.com>
2019-06-21 19:38:53 +0800

    basic example to login.

---
+++ example/example_app_component.dart
@@ -0,0 +1,57 @@
+import 'package:angular/angular.dart';
+import 'package:angular_components/material_button/material_button.dart';
+
+import 'package:keycloak_dart/keycloak.dart';
+
+@Component(
+  selector: 'my-app',
+  directives: [MaterialButtonComponent, NgIf],
+  providers: [ClassProvider(KeycloakService)],
+  template: '''
+    <h1>Keycloak example</h1>
+
+    <div style="padding: 8px;" [innerHtml]="info">
+    </div>
+
+    <material-button raised *ngIf="!isAuthenticated" (trigger)="login">
+      Login
+    </material-button>
+    <material-button raised *ngIf="isAuthenticated" (trigger)="logout">
+      Logout
+    </material-button>
+  ''',
+)
+class ExampleAppComponent implements OnInit {
+  final KeycloakService _keycloakService;
+
+  bool get isAuthenticated => _keycloakService.isAuthenticated;
+
+  String get info {
+    if (isAuthenticated) {
+      return 'Logged in';
+    } else {
+      return '''
+      Server: ${_keycloakService.authServerUrl}
+      <br>
+      Realm: ${_keycloakService.realm}
+      <br>
+      Client ID: ${_keycloakService.clientId} 
+      ''';
+    }
+  }
+
+  ExampleAppComponent(this._keycloakService);
+
+  @override
+  void ngOnInit() async {
+    await _keycloakService.init();
+  }
+
+  void login() {
+    _keycloakService.login();
+  }
+
+  void logout() {
+    _keycloakService.logout();
+  }
+}
--- lib/src/keycloak_service.dart
+++ lib/src/keycloak_service.dart
@@ -4,21 +4,26 @@ import 'js_interop/promise.dart';
 class KeycloakService {
   KeycloakInstance<Promise> _keycloakInstance;
 
+  bool get isAuthenticated => _keycloakInstance.authenticated;
+
+  String get authServerUrl => _keycloakInstance.authServerUrl;
+  String get clientId => _keycloakInstance.clientId;
+  String get realm => _keycloakInstance.realm;
+
   KeycloakService([config]) {
     _keycloakInstance = Keycloak(config);
   }
 
   Future init() async {
-    await promiseToFuture(
+    return promiseToFuture(
         _keycloakInstance.init(KeycloakInitOptions(promiseType: 'native')));
-
-    print('authenticated? ${_keycloakInstance.authenticated}');
-    print('loginRequired? ${_keycloakInstance.loginRequired}');
-    print('realm? ${_keycloakInstance.realm}');
-    return;
   }
 
   Future login([KeycloakLoginOptions options]) async {
     return promiseToFuture(_keycloakInstance.login(options));
+  }
+
+  Future logout() async {
+    return promiseToFuture(_keycloakInstance.logout());
   }
 }
