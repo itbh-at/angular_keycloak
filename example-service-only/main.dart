2077dc153267391f92e15e6361a2ae27bcb41fe2
kuan <yuankuan@gmail.com>
2019-07-14 18:03:15 +0800

    another example for simpler use case demo

---
+++ example-service-only/main.dart
@@ -0,0 +1,13 @@
+import 'package:angular/angular.dart';
+
+import 'package:angular_keycloak/angular_keycloak.dart';
+
+import 'app/example_app_component.template.dart' as ng;
+import 'main.template.dart' as self;
+
+@GenerateInjector([keycloakProviders])
+final InjectorFactory injector = self.injector$Injector;
+
+void main() {
+  runApp(ng.ExampleAppComponentNgFactory, createInjector: injector);
+}
