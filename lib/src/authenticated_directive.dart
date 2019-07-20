5a54952a43512707832ca17716828e55b819af62
kuan <yuankuan@gmail.com>
2019-07-20 15:25:32 +0800

    Documentation

--- lib/src/authenticated_directive.dart
+++ lib/src/authenticated_directive.dart
@@ -2,6 +2,24 @@ import 'package:angular/angular.dart';
 
 import 'keycloak_service.dart';
 
+/// Causes an element and its contents to be conditionally added/removed from
+/// the DOM based on the authentication status with [KeycloakService].
+///
+/// Instance ID of specific [KeycloakInstance] can be provided,
+/// of leave blank for single instance usage.
+///
+/// Following example show how to show a <p> element when authenticated.
+///
+/// ```'html'
+/// <p *authenticated>Welcome!</p>
+/// ```
+///
+/// Following example show how to show a <div> element when authenticated
+/// with a specific instance via a getter function.
+///
+/// ```'html
+/// <div *authenticated="getInstanceId">User Only</div>
+/// ```
 @Directive(
   selector: '[authenticated]',
 )
@@ -11,11 +29,13 @@ class AuthenticatedDirective implements DoCheck {
   final ViewContainerRef _viewContainer;
 
   String _instanceId;
-  bool _hasView = false;
+  bool _checked = false;
   bool showIfAuthenticate = true;
 
   @Input('authenticated')
   set instanceId(String instanceId) {
+    // It seems that @Input never pass in a `null` String, even given one.
+    // It will always became an empty String.
     _instanceId = instanceId.isNotEmpty ? instanceId : null;
   }
 
@@ -24,7 +44,7 @@ class AuthenticatedDirective implements DoCheck {
 
   @override
   void ngDoCheck() {
-    if (!_hasView) {
+    if (!_checked) {
       final show =
           (_keycloakService.isInstanceInitiated(instanceId: _instanceId) &&
                   _keycloakService.isAuthenticated(instanceId: _instanceId)) ==
@@ -34,11 +54,13 @@ class AuthenticatedDirective implements DoCheck {
       } else {
         _viewContainer.clear();
       }
-      _hasView = true;
+      _checked = true;
     }
   }
 }
 
+/// The opposite of [AuthenticatedDirective], this hide the element when
+/// [KeycloakService] is not authenticated.
 @Directive(
   selector: '[notAuthenticated]',
 )
@@ -49,8 +71,8 @@ class NotAuthenticatedDirective extends AuthenticatedDirective {
     showIfAuthenticate = false;
   }
 
-  @Input('notAuthenticated')
-  set instanceId_(String instanceId) {
+  @Input()
+  set notAuthenticated(String instanceId) {
     super.instanceId = instanceId;
   }
 }
