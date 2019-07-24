5fe9ce19719320264c3f528d12ba6a8fc45f242b
kuan <yuankuan@gmail.com>
2019-07-24 23:49:32 +0800

    Documentation for directive

--- lib/src/security_directive.dart
+++ lib/src/security_directive.dart
@@ -3,43 +3,74 @@ import 'package:angular/angular.dart';
 import 'keycloak_service.dart';
 
 /// Causes an element and its contents to be added at the DOM
-/// based on the authorization status with [KeycloakService].
+/// based on the authentication and authorization status with [KeycloakService].
 ///
-/// Instance ID of specific [KeycloakInstance] can be provided,
-/// of leave blank for single instance usage.
+/// Instance ID of specific [KeycloakInstance] can be provided in the
+/// [Microsyntax](https://angulardart.dev/guide/structural-directives#microsyntax).
+/// Or leave blank for single instance usage.
 ///
-/// /// Following example show how to show a <p> element when authenticated.
+/// Following example show how to display a <p> element when authenticated.
 ///
 /// ```'html'
-/// <p *authenticated>Welcome!</p>
+/// <p *kcSecurity>Welcome!</p>
 /// ```
 ///
-/// Following example show how to show a <div> element when authenticated
-/// with a specific instance via a getter function.
+/// Following example show how to display a <div> element when authenticated
+/// with a specific instance.
 ///
 /// ```'html
-/// <div *authenticated="getInstanceId">User Only</div>
+/// <div *kcSecurity="getInstanceId">User Only</div>
 /// ```
 ///
-/// Roles are `<String>[]` to be provided via the `roles`.
-/// Unauthorized, i.e. does not has the correct roles, will not add
-/// the content to DOM.
+/// Roles are `<String>[]` to be provided via the `roles`. When roles were defined
+/// the content will be shown if only user is autenticated and authorized.
+/// i.e. having the correct roles assigned to user's Keycloak profile.
+///
+/// Following example show how to display a <div> element when authenticated
+/// and authorized with the role of 'vip'.
 ///
 /// ```'html'
-/// <div *authorized="roles: ['vip']"></div>
+/// <div *kcSecurity="roles: ['vip']">
+///   VIP will see this, other will not.
+/// </div>
 /// ```
 ///
-/// Readonly roles are `<String>[]` will be via `readonlyRoles`.
+/// Readonly roles are `<String>[]` to be provided via `readonlyRoles`.
 ///
 /// When readonly roles are provided, a local variable can be acquire
-/// with `let ro=readonly`. It will be true if the user's authorization
+/// with `let value=readonly`. `readonly` will be true if the user's authorization
 /// has roles in the `readonlyRoles` but not in the actual `roles`.
 ///
+/// Following example show how to display a <div> element when authenticated
+/// and authorized with the role of 'supervisor' or 'boss'. If user has only 'supervisor'
+/// role, `ro` will be true. And in this example, the <input> will become a readonly
+/// text input.
+///
 /// ```'html'
-/// <div *authorized="readonlyRoles: ['supervisor'];
+/// <div *kcSecurity="readonlyRoles: ['supervisor'];
 ///                   roles: ['boss'];
 ///                   let ro = readonly">
 ///   <input [readonly]="ro" type="text" />
+/// </div>
+/// ```
+///
+/// All the examples above show how to show the content when access is granted.
+/// The opposite, i.e. when access is denied, `showWhenDenied` can flag as true.
+/// In that case, the content will be shown when authentication or authorization failed.
+///
+/// Following example show how to display the login button when unauthenticated
+///
+/// ```'html`
+/// <material-button  *kcSecurity="showWhenDenied: true">
+///   Login
+/// </material-button>
+/// ```
+///
+/// Following example show how to display a warning when user is not authorized.
+///
+/// ```'html'
+/// <div *kcSecurity="roles: ['vip']; showWhenDenied: true">
+///   You are not authorized to enter the VIP room.
 /// </div>
 /// ```
 @Directive(selector: '[kcSecurity]')
@@ -89,8 +120,7 @@ class KcSecurity implements DoCheck {
           } else {
             shouldAddContent = true;
           }
-        } else if (_isAuthorized(_readonlyRoles)) {
-          //TODO: We made an assumption that if you are not authorize for readonly, you can't authorize for write.
+        } else if (_isAuthorized(_readonlyRoles) || _isAuthorized(_roles)) {
           shouldAddContent = true;
           shouldSetReadonly = _roles.isNotEmpty && !_isAuthorized(_roles);
         }
