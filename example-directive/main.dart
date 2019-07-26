64a7c8ea51da0f29404fc588d72b43f25c1f1318
kuan <yuankuan@gmail.com>
2019-07-26 23:58:42 +0800

    Documentation for example's main.dart

--- example-directive/main.dart
+++ example-directive/main.dart
@@ -5,7 +5,10 @@ import 'package:angular_keycloak/angular_keycloak.dart';
 import 'app/example_app_component.template.dart' as ng;
 import 'main.template.dart' as self;
 
-//TODO Description
+/// Example for using the [kcSecurity] structural directive.
+///
+/// Most of the important bits of this example is at the
+/// `app/example_app_component.html` file.
 
 @GenerateInjector([keycloakProviders])
 final InjectorFactory injector = self.injector$Injector;
