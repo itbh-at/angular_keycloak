3f5eb63a469663e7ea0c25c1b3fc0db89531f327
kuan <yuankuan@gmail.com>
2019-08-01 13:25:20 +0800

    Tests for sub route security

--- test/secured_router_hook_test.dart
+++ test/secured_router_hook_test.dart
@@ -18,8 +18,7 @@ void main() {
   setUp(() async {
     final testBed = NgTestBed.forComponent(ng.TestAppComponentNgFactory)
         .addInjector(createInjector);
-    final testFixture =
-        await testBed.create(beforeComponentCreated: (injector) {
+    await testBed.create(beforeComponentCreated: (injector) {
       _router = injector.provideType(Router);
       _mockKeycloakService =
           injector.provideType(KeycloakService) as MockKeycloakService;
@@ -41,12 +40,19 @@ void main() {
 
     await _router.navigate(RoutePaths.customer.toUrl());
 
-    final capturedInstanceId = verify(
-            _mockKeycloakService.initWithProvidedConfig(
-                instanceId: captureAnyNamed('instanceId'),
-                redirectedOrigin: anyNamed('redirectedOrigin')))
+    var capturedInstanceId = verify(_mockKeycloakService.initWithProvidedConfig(
+            instanceId: captureAnyNamed('instanceId'),
+            redirectedOrigin: anyNamed('redirectedOrigin')))
         .captured[0];
     expect(capturedInstanceId, 'customer');
+
+    await _router.navigate(RoutePaths.employee.toUrl());
+
+    capturedInstanceId = verify(_mockKeycloakService.initWithProvidedConfig(
+            instanceId: captureAnyNamed('instanceId'),
+            redirectedOrigin: anyNamed('redirectedOrigin')))
+        .captured[0];
+    expect(capturedInstanceId, 'employee');
   });
 
   test('redirect to SecuredRoute.redirectPath when not authenticated',
@@ -65,8 +71,7 @@ void main() {
     expect(_location.path(), RoutePaths.customerLogin.toUrl());
   });
 
-  test('block when SecuredRoute.redirectPath is undefined and not authorized',
-      () async {
+  test('redirect to SecuredRoute.redirectPath when not authorized', () async {
     when(_mockKeycloakService.isInstanceInitiated(
             instanceId: anyNamed('instanceId')))
         .thenReturn(true);
@@ -81,7 +86,8 @@ void main() {
 
     final navigationResult = await _router.navigate(RoutePaths.vip.toUrl());
 
-    expect(navigationResult, NavigationResult.BLOCKED_BY_GUARD);
+    expect(navigationResult, NavigationResult.SUCCESS);
+    expect(_location.path(), RoutePaths.unauthorized.toUrl());
   });
 
   test('proceed with path when authenticated', () async {
@@ -122,6 +128,66 @@ void main() {
     expect(navigationResult, NavigationResult.SUCCESS);
     expect(_location.path(), RoutePaths.vip.toUrl());
   });
+
+  group('Sub routes', () {
+    test(
+        'access denied when visit deeper sub routes but not authenticated by parent route',
+        () async {
+      when(_mockKeycloakService.isInstanceInitiated(
+              instanceId: anyNamed('instanceId')))
+          .thenReturn(true);
+      when(_mockKeycloakService.isAuthenticated(instanceId: 'employee'))
+          .thenReturn(false);
+
+      final navigationResult =
+          await _router.navigate(RoutePaths.cashier.toUrl());
+
+      expect(navigationResult, NavigationResult.BLOCKED_BY_GUARD);
+    });
+
+    test(
+        'access denied when visit deeper sub routes but not authorized by parent route',
+        () async {
+      when(_mockKeycloakService.isInstanceInitiated(
+              instanceId: anyNamed('instanceId')))
+          .thenReturn(true);
+      when(_mockKeycloakService.isAuthenticated(instanceId: 'employee'))
+          .thenReturn(true);
+      when(_mockKeycloakService.getRealmRoles(
+              instanceId: anyNamed('instanceId')))
+          .thenReturn([]);
+      when(_mockKeycloakService.getResourceRoles(
+              instanceId: anyNamed('instanceId')))
+          .thenReturn(['staff']);
+
+      final navigationResult =
+          await _router.navigate(RoutePaths.bossRoom.toUrl());
+
+      expect(navigationResult, NavigationResult.BLOCKED_BY_GUARD);
+    });
+
+    test(
+        'access granted when visit deeper sub routes when authenticated and authorized',
+        () async {
+      when(_mockKeycloakService.isInstanceInitiated(
+              instanceId: anyNamed('instanceId')))
+          .thenReturn(true);
+      when(_mockKeycloakService.isAuthenticated(instanceId: 'employee'))
+          .thenReturn(true);
+      when(_mockKeycloakService.getRealmRoles(
+              instanceId: anyNamed('instanceId')))
+          .thenReturn([]);
+      when(_mockKeycloakService.getResourceRoles(
+              instanceId: anyNamed('instanceId')))
+          .thenReturn(['staff', 'supervisor']);
+
+      final navigationResult =
+          await _router.navigate(RoutePaths.cashier.toUrl());
+
+      expect(navigationResult, NavigationResult.SUCCESS);
+      expect(_location.path(), RoutePaths.cashier.toUrl());
+    });
+  });
 }
 
 class RoutePaths {
@@ -160,11 +226,23 @@ class TestAppComponent {
         component: ng.CustomerLoginComponentNgFactory),
     RouteDefinition(
         routePath: RoutePaths.customer,
-        component: ng.CustomerComponentNgFactory)
+        component: ng.CustomerComponentNgFactory),
+    RouteDefinition(
+        routePath: RoutePaths.employeeLogin,
+        component: ng.EmployeeLoginComponentNgFactory),
+    RouteDefinition(
+        routePath: RoutePaths.employee,
+        component: ng.EmployeeComponentNgFactory),
+    RouteDefinition(
+        routePath: RoutePaths.unauthorized,
+        component: ng.UnauthorizedComponentNgFactory)
   ];
 
   TestAppComponent(this.router);
 }
+
+@Component(selector: 'unauthorized', template: '<h1>Unauthorized</h1>')
+class UnauthorizedComponent {}
 
 @Component(selector: 'customer-login', template: '<h1>customer login</h1>')
 class CustomerLoginComponent {}
@@ -182,6 +260,48 @@ class CustomerComponent {
 
 @Component(selector: 'vip', template: '<h1>vip</h1>')
 class VipComponent {}
+
+@Component(selector: 'employee-login', template: '<h1>employee login</h1>')
+class EmployeeLoginComponent {}
+
+@Component(selector: 'employee', directives: [RouterOutlet], template: '''
+    <h1>employee</h1>
+    <router-outlet [routes]="routes"></router-outlet>
+    ''')
+class EmployeeComponent {
+  static final routes = [
+    RouteDefinition(
+        routePath: RoutePaths.kitchen, component: ng.KitchenComponentNgFactory),
+  ];
+}
+
+@Component(selector: 'kitchen', directives: [RouterOutlet], template: '''
+    <h2>kitchen</h2>
+    <router-outlet [routes]="routes"></router-outlet>
+    ''')
+class KitchenComponent {
+  static final routes = [
+    RouteDefinition(
+        routePath: RoutePaths.cashier, component: ng.CashierComponentNgFactory),
+  ];
+}
+
+@Component(selector: 'cashier', directives: [RouterOutlet], template: '''
+    <h3>cashier</h3>
+    <router-outlet [routes]="routes"></router-outlet>
+    ''')
+class CashierComponent {
+  static final routes = [
+    RouteDefinition(
+        routePath: RoutePaths.bossRoom,
+        component: ng.BossRoomComponentNgFactory),
+  ];
+}
+
+@Component(selector: 'boss-room', directives: [RouterOutlet], template: '''
+    <h4>Boss Room</h4>
+    ''')
+class BossRoomComponent {}
 
 KeycloackServiceConfig keycloakConfigFactory(
         LocationStrategy _locationStrategy) =>
@@ -205,21 +325,18 @@ SecuredRouterHookConfig securedRouterHookConfigFactory() =>
       SecuredRoute.authorization(
           keycloakInstanceId: 'customer',
           paths: [RoutePaths.vip],
-          authorizedRoles: ['vip']),
+          authorizedRoles: ['vip'],
+          redirectPath: RoutePaths.unauthorized),
       SecuredRoute.authentication(
-          keycloakInstanceId: 'employee',
-          paths: [RoutePaths.employee],
-          redirectPath: RoutePaths.employeeLogin),
+          keycloakInstanceId: 'employee', paths: [RoutePaths.employee]),
       SecuredRoute.authorization(
           keycloakInstanceId: 'employee',
           paths: [RoutePaths.cashier],
-          authorizedRoles: ['staff', 'supervisor'],
-          redirectPath: RoutePaths.unauthorized),
+          authorizedRoles: ['staff', 'supervisor']),
       SecuredRoute.authorization(
           keycloakInstanceId: 'employee',
           paths: [RoutePaths.bossRoom],
-          authorizedRoles: ['boss'],
-          redirectPath: RoutePaths.unauthorized)
+          authorizedRoles: ['boss'])
     ]);
 
 class MockKeycloakService extends Mock implements KeycloakService {}
